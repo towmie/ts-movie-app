e946f3b2754011b2fde67298bc9364e8b163ad6e
towmie <andre00toz@gmail.com>
2024-06-04 12:38:58 +0300

    feat: finish: sidebar

--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -1,4 +1,9 @@
-import { Box } from "@mui/material";
+import { Box, Hidden, Typography } from "@mui/material";
+import { BiCameraMovie } from "react-icons/bi";
+import { FaRegBookmark } from "react-icons/fa";
+import { PiSquaresFour, PiSquaresFourThin } from "react-icons/pi";
+import { RiMovie2Line } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 const navLinks = [
   {
@@ -40,7 +45,71 @@ export default function Sidebar() {
         },
       }}
     >
-      Sidebar
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: {
+            xs: "row",
+            lg: "column",
+          },
+          gap: 5,
+          alignItems: {
+            xs: "center",
+            lg: "start",
+          },
+          width: "100%",
+        }}
+      >
+        <Box sx={{ display: { md: "none", lg: "block" } }}>
+          <Typography
+            variant="h5"
+            component="h1"
+            my={2}
+            fontWeight={400}
+            fontSize={18}
+          >
+            Movie App
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            py: {
+              xs: "0px",
+              lg: "16px",
+            },
+            display: "flex",
+            flexDirection: {
+              xs: "row",
+              lg: "column",
+            },
+            gap: 4,
+          }}
+        >
+          {navLinks.map((link) => (
+            <Link
+              key={link.name}
+              to={link.link}
+              style={{ fontSize: "16px", textDecoration: "none" }}
+            >
+              <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+                <Box sx={{ width: "20px", height: "20px" }}>
+                  {link.name === "Home" && <PiSquaresFour />}
+                  {link.name === "Movies" && <RiMovie2Line />}
+                  {link.name === "TV-series" && <BiCameraMovie />}
+                  {link.name === "Bookmarks" && <FaRegBookmark />}
+                </Box>
+                <Box
+                  sx={{
+                    display: { md: "none", lg: "block", fontSize: "16px" },
+                  }}
+                >
+                  <Typography fontSize={16}>{link.name}</Typography>
+                </Box>
+              </Box>
+            </Link>
+          ))}
+        </Box>
+      </Box>
     </Box>
   );
 }
